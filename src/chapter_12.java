022716308168d32f51d2da76a0864e6fc724a0cd
shaheer <ssm2591@vccs.email.edu>
2018-02-01 13:46:46 -0500

    Initial commit

---
+++ src/chapter_12.java
@@ -0,0 +1,37 @@
+/*********************************************************************************
+
+ * (OutOfMemoryError) Write a program that causes the JVM to throw an             *
+
+ * OutOfMemoryError and catches and handles this error.                           *
+
+ *********************************************************************************/
+
+public class chapter_12 {
+
+    /**  Main method */
+
+    public static void main(String[] args) {
+
+        // Create an array that exceeds heap space
+
+        int length = 1200000000;
+
+        try {
+
+            byte[] array = new byte[length];
+
+        }
+
+        catch (OutOfMemoryError ex) { // Handle OutOfMemoryError
+
+            System.out.println(ex.getMessage());
+
+        }
+
+        // Continue program
+
+        System.out.println("Error handled program continues... ");
+
+    }
+
+}
